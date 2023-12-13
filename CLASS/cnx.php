9c7639aca4414bf9ca5ae9e172937506eb2b14f6
anouar <anouar.ab95@gmail.com>
2023-12-13 21:14:47 +0100

    MODIFER Page cnx

--- CLASS/cnx.php
+++ CLASS/cnx.php
@@ -10,16 +10,16 @@ $con = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
 
 
  
-  require_once __DIR__."./utilisateur.php";
-  $Utilisateur = new Utilisateur($con);
- require_once __DIR__."./login.php";
- $login= new Login($con);
- require_once "validation.php";
- $validation= new Validation($con);
- require_once "clientclas.php"; 
- $client= new Client($con);
- require_once "categorie.php"; 
- $Categorie = new Categorie($con);
+require_once __DIR__."./utilisateur.php";
+$Utilisateur = new Utilisateur($con);
+require_once __DIR__."./login.php";
+$login= new Login($con);
+require_once "validation.php";
+$validation= new Validation($con);
+require_once "clientclas.php"; 
+$client= new Client($con);
+require_once "categorie.php"; 
+$Categorie = new Categorie($con);
 
    
 
