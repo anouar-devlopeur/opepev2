402b1d23fba9c3129919f8f03a3d8471d2de6818
anouar <anouar.ab95@gmail.com>
2023-12-14 10:51:28 +0100

    finich page client

--- client.php
+++ client.php
@@ -3,9 +3,22 @@
 <?php 
 include './CLASS/cnx.php';
 session_start();
+if (empty($_SESSION['idUtl'])|| isset($_POST['logout'])) {
+   
+    $_SESSION['idUtl'] = "";
+
+
+    session_destroy();
+
+   
+    header('location: index.php');
+    
+
+    exit();
+}
 $userId = $_SESSION['idUtl'];
 $count=$client->countplant($userId);
-//  echo''.$count.'';
+
 
 
 ?>
@@ -202,14 +215,14 @@ $count=$client->countplant($userId);
 
                          <!-- log out -->
                         <li>
-                        <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" >
-                          <a href="index.php">
+                        <form method="post"  >
+                          <button class=" btn-success" name="logout">
                           <i class="ri-logout-box-r-line" style="font-size:27px;"></i>
-                        </a>
+                        </button>
                         </form>
                       </li>
                       <li >
-                    <a href="panier.php" class="d-flex">
+                    <a href="pannierp.php" class="d-flex">
                         <svg xmlns="http://www.w3.org/2000/svg"  width="30" height="30" fill="currentColor" class="bi bi-cart3" viewBox="0 0 16 16">
                         <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
                         </svg>
